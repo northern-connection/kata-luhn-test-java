1be44c0cb1847f540c17067ea293e058c07a1eae
Antonio de la Torre <antonio@codesai.com>
2018-01-23 09:22:33 +0100

    Init project

---
+++ src/test/java/luhn_test/LuhnTestTest.java
@@ -0,0 +1,15 @@
+package luhn_test;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class LuhnTestTest {
+
+    @Test
+    public void test_works() {
+        assertThat(true, is(false));
+    }
+}
