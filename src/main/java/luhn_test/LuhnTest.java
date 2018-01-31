ccd7aecdeab083e7dfc307f209b14971a0da623f
Antonio de la Torre <antonio@codesai.com>
2018-01-31 09:57:01 +0100

    Refactor: Separating responsabilities by breaking the loop in three

--- src/main/java/luhn_test/LuhnTest.java
+++ src/main/java/luhn_test/LuhnTest.java
@@ -1,15 +1,29 @@
 package luhn_test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LuhnTest {
     public static boolean isValid(String digits) {
         String reversedDigits = new StringBuilder(digits).reverse().toString();
 
-        int acum = 0;
+        List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < reversedDigits.length() ; i++) {
+            String digit = reversedDigits.substring(i, i + 1);
+            numbers.add(Integer.parseInt(digit));
+        }
+
+        List<Integer> odds = new ArrayList<>();
+        for (int i = 0; i < numbers.size() ; i++) {
             int pos = i + 1;
             if (pos % 2 != 0) {
-                acum += Integer.parseInt(reversedDigits.substring(i, i + 1));
+                odds.add(numbers.get(i));
             }
+        }
+
+        int acum = 0;
+        for (Integer odd : odds) {
+            acum += odd;
         }
 
         return acum % 10 == 0;
