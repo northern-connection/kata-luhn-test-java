1191a053e75209e7085d96e416df3226961d913d
Antonio de la Torre <antonio@codesai.com>
2018-01-31 10:00:59 +0100

    Refactor: Extract helper methods

--- src/main/java/luhn_test/LuhnTest.java
+++ src/main/java/luhn_test/LuhnTest.java
@@ -5,14 +5,27 @@ import java.util.List;
 
 public class LuhnTest {
     public static boolean isValid(String digits) {
-        String reversedDigits = new StringBuilder(digits).reverse().toString();
+        String reversedDigits = reverseDigits(digits);
+        List<Integer> numbers = getNumbers(reversedDigits);
+        List<Integer> odds = filterOdds(numbers);
+        int acum = sum(odds);
 
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < reversedDigits.length() ; i++) {
-            String digit = reversedDigits.substring(i, i + 1);
-            numbers.add(Integer.parseInt(digit));
+        return acum % 10 == 0;
+    }
+
+    private static String reverseDigits(String digits) {
+        return new StringBuilder(digits).reverse().toString();
+    }
+
+    private static int sum(List<Integer> numbers) {
+        int acum = 0;
+        for (Integer number : numbers) {
+            acum += number;
         }
+        return acum;
+    }
 
+    private static List<Integer> filterOdds(List<Integer> numbers) {
         List<Integer> odds = new ArrayList<>();
         for (int i = 0; i < numbers.size() ; i++) {
             int pos = i + 1;
@@ -20,12 +33,15 @@ public class LuhnTest {
                 odds.add(numbers.get(i));
             }
         }
+        return odds;
+    }
 
-        int acum = 0;
-        for (Integer odd : odds) {
-            acum += odd;
+    private static List<Integer> getNumbers(String digits) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < digits.length() ; i++) {
+            String digit = digits.substring(i, i + 1);
+            numbers.add(Integer.parseInt(digit));
         }
-
-        return acum % 10 == 0;
+        return numbers;
     }
 }
