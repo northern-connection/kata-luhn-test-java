83fe9a5bb7d5bb2491862beb03bfc4a6c8d7abe4
Antonio de la Torre <antonio@codesai.com>
2018-03-08 11:41:18 +0100

    Refactor: use collection pipeline for numbers at odd positions.

--- src/main/java/luhn_test/LuhnTest.java
+++ src/main/java/luhn_test/LuhnTest.java
@@ -8,24 +8,20 @@ public class LuhnTest {
     public static boolean isValid(String digits) {
         String reversedDigits = reverseDigits(digits);
         List<Integer> numbers = getNumbers(reversedDigits);
-        List<Integer> numbersAtOddPositions = extractNumbersAtPositions((pos) -> pos % 2 != 0, numbers);
 
-        int evenPositionContribution = extractNumbersAtPositions((pos) -> pos % 2 == 0, numbers).stream()
+        int oddPositionsContribution = extractNumbersAtPositions((pos) -> pos % 2 != 0, numbers).stream()
+                .reduce(0, Integer::sum);
+
+        int evenPositionsContribution = extractNumbersAtPositions((pos) -> pos % 2 == 0, numbers).stream()
                 .map(n -> n * 2)
                 .map(n -> n % 10 + n / 10)
                 .reduce(0, Integer::sum);
 
-        int result = sum(numbersAtOddPositions) + evenPositionContribution;
-
-        return result % 10 == 0;
+        return (oddPositionsContribution + evenPositionsContribution) % 10 == 0;
     }
 
     private static String reverseDigits(String digits) {
         return new StringBuilder(digits).reverse().toString();
-    }
-
-    private static int sum(List<Integer> numbers) {
-        return numbers.stream().reduce(0, Integer::sum);
     }
 
     private static List<Integer> extractNumbersAtPositions(IntPredicate predicate, List<Integer> numbers) {
